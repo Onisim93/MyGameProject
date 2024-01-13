050350f6015215c4e60e422c4be44a52564b80db
Onisim Albarov <Onisim93@gmail.com>
2024-01-13 12:47:33 +0400

    first commit

---
+++ src/main/java/org/example/MyGame.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import com.javarush.engine.cell.Game;
+
+public class MyGame extends Game {
+
+    @Override
+    public void initialize() {
+        setScreenSize(50, 50);
+    }
+}
